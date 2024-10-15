6403cb81df53294613d0473fe8bd2474c085b66e
Oleksandr Khytrov <o.khytrov@gmail.com>
2024-10-15 18:04:07 +0300

    Added CreteOrderRequest

---
+++ Bank.Api/Models/CreteOrderRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using Bank.Common;
+
+namespace Bank.Api.Models;
+
+public class CreteOrderRequest
+{
+    [JsonPropertyName("client_id")]
+    public string ClientId { get; set; }
+
+    [JsonPropertyName("department_address")]
+    public string DepartmentAddress { get; set; }
+
+    [JsonPropertyName("amount")]
+    public decimal Amount { get; set; }
+
+    [JsonPropertyName("currency")]
+    public Currency Currency { get; set; }
+}
